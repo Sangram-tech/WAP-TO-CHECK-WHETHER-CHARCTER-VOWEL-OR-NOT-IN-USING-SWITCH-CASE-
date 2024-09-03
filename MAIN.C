afd34932af2bf3cea9387d66dfe84774e0b06878
Sangram-tech <sangrammahakud1@gmail.com>
2024-09-03 20:16:08 +0530

    Add files via upload

---
+++ MAIN.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    char c;
+    printf("enter the character::");
+    scanf("%c",&c);
+    switch(c)
+    {
+        case-'a':
+        printf("vowel");
+        break;
+        case-'e':
+        printf("vowel");
+        break;
+        case-'i':
+        printf("vowel");
+        break;
+        case-'o':
+        printf("vowel");
+        break;
+        case-'u':
+        printf("vowel");
+        break;
+        default:
+        printf("consonant");
+
+    }
+}
